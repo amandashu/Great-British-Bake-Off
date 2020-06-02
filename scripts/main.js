acde22e98acc66170bf744284439fe26f6726119
amandashu <shu.amanda25@gmail.com>
2020-06-02 11:05:07 -0700

    add bubble chart with baker's images

--- scripts/main.js
+++ scripts/main.js
@@ -213,6 +213,180 @@ function plotWordCloud(){
         // }
     });
 }
+
+function plotBubble(){
+	// var width = 500, height = 500;
+	// var svg = d3.select('#myBubbleChart')
+	// 	.append('svg')
+	// 	.attr('height', height)
+	// 	.attr('width',width)
+	// 	.append('g')
+	// 	.attr("transform","translate(" + width/2 + "," + height/2 +")") // translate to center
+
+	// var defs = svg.append("defs") // add defs in svg
+	// defs.append("pattern") //add pattern in defs
+	// 	.attr("id","test")
+	// 	.attr("height","100%")
+	// 	.attr("weight","100%")
+	// 	.attr("patternContentUnits","objectBoundingBox")
+	// 	.append("image") //image in the pattern
+	// 	.attr("height",1)
+	// 	.attr("width",1)
+	// 	.attr("preserveAspectRatio","none")
+	// 	.attr("xmlns:xlink","http://www.w3.org/1999/xlink")
+	// 	.attr("xlink:href","images/frances.jpg");
+
+
+	// var radiusScale = d3.scaleSqrt()
+	// 	.domain([1,500]) //domain of data
+	// 	.range([10,80]) // radius of circles
+
+	// var simulation = d3.forceSimulation()
+	// 	.force("x", d3.forceX().strength(0.05)) // stay at center
+	// 	.force("y", d3.forceY().strength(0.05))
+	// 	.force("collide",d3.forceCollide(function(d){
+	// 		return radiusScale(d.followers) +1//pull circles apart based on radius of circle
+	// 	})) 
+
+	// d3.queue()
+	// 	.defer(d3.csv, "data/insta.csv")
+	// 	.await(ready)
+
+	// function ready (error, datapoints){
+	// 	//create patterns for circles
+	// 	defs.selectAll(".baker-pattern")
+	// 	.data(datapoints)
+	// 	.enter().append("pattern")
+	// 	.attr("class", "baker-pattern")
+	// 	.attr("id","test")
+
+	// 	//draw circles
+	// 	var circles = svg.selectAll(".artist")
+	// 		.data(datapoints)
+	// 		.enter().append("circle")
+	// 		.attr("class","artist")
+	// 		.attr("r",function(d){
+	// 			return radiusScale(d.followers)
+	// 		})
+	// 		.attr("fill","lightblue")
+	// 		.attr("stroke","black")
+
+
+	// simulation.nodes(datapoints)
+	// 	.on("tick",ticked)
+
+	// function ticked(){
+	// 	circles.
+	// 		attr("cx",function(d){
+	// 			return d.x
+	// 		})
+	// 		.attr("cy", function(d){
+	// 			return d.y
+	// 		})
+	// }
+	// }
+
+	var width = 500, height = 500;
+	var svg = d3.select('#myBubbleChart')
+		.append('svg')
+		.attr('height', height)
+		.attr('width',width)
+		.append('g')
+		.attr("transform","translate(" + width/2 + "," + height/2 +")") // translate to center
+
+	var defs = svg.append("defs") // add defs in svg
+	defs.append("pattern") //add pattern in defs
+		.attr("id","test")
+		.attr("height","100%")
+		.attr("weight","100%")
+		.attr("patternContentUnits","objectBoundingBox")
+		.append("image") //image in the pattern
+		.attr("height",1)
+		.attr("width",1)
+		.attr("preserveAspectRatio","none")
+		.attr("xmlns:xlink","http://www.w3.org/1999/xlink")
+		.attr("xlink:href","images/frances.jpg");
+
+
+	var radiusScale = d3.scaleSqrt()
+		.domain([1,500]) //domain of data
+		.range([10,80]) // radius of circles
+
+	var simulation = d3.forceSimulation()
+		.force("x", d3.forceX().strength(0.05)) // stay at center
+		.force("y", d3.forceY().strength(0.05))
+		.force("collide",d3.forceCollide(function(d){
+			return radiusScale(d.followers) +1//pull circles apart based on radius of circle
+		})) 
+
+	d3.queue()
+		.defer(d3.csv, "data/insta.csv")
+		.await(ready)
+
+	function ready (error, datapoints){
+		// example html
+		// <svg viewBox="0 0 300 100">
+		// 	<defs>
+		// 	<pattern id="test" height = "100%" width = "100%"            
+		// 	patternContentUnits = "objectBoundingBox">
+		// 		<image x="0" y="0" height=1 width=1 href="images/edd.jpg"></image>
+		// 	</pattern>
+		// 	</defs>
+		
+		// 	<circle cx="150" cy="50" r="40" fill="url(#test)" />
+		// </svg>
+
+		//create patterns for circles
+		defs.selectAll(".baker-pattern")
+		.data(datapoints)
+		.enter().append("pattern")
+		.attr("class", "baker-pattern")
+		.attr("id",function(d) {
+			return d.baker.toLowerCase()
+		})
+		.attr("height","100%")
+		.attr("width","100%")
+		.attr("patternContentUnits","objectBoundingBox")
+		.append("image") //image in the pattern
+		.attr("height",1)
+		.attr("width",1)
+		.attr("preserveAspectRatio","none")
+		.attr("xmlns:xlink","http://www.w3.org/1999/xlink")
+		.attr("xlink:href",function(d){
+			return d.image_path
+		});
+
+		//draw circles
+		var circles = svg.selectAll(".artist")
+			.data(datapoints)
+			.enter().append("circle")
+			.attr("class","artist")
+			.attr("r",function(d){
+				return radiusScale(d.followers)
+			})
+			.attr("fill",function(d){
+				return "url(#" + d.baker.toLowerCase() +")"
+			})
+			.attr("stroke","black")
+
+
+	simulation.nodes(datapoints)
+		.on("tick",ticked)
+
+	function ticked(){
+		circles.
+			attr("cx",function(d){
+				return d.x
+			})
+			.attr("cy", function(d){
+				return d.y
+			})
+	}
+	}
+
+}
+
+
 function plotStackColumn(){
     let myStackedColumn = Highcharts.chart('myStackedColumn', {
         chart: {
@@ -309,7 +483,8 @@ function init() {
     wordsPromise.then(function (w){
         words = w;
         plotWordCloud();
-    });
+	});
+	plotBubble();
     statsPromise.then(function (s) {
         winnerStats = s
 		plotStackColumn();
