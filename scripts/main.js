93f5b5840ebcb10b13e3c2252df14f5e6c4f0bd9
amandashu <shu.amanda25@gmail.com>
2020-06-02 23:41:51 -0700

    change bake image scales, make header components resize when window resizes, and add top bar

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,10 @@
+var bcolor = '#eaf1f8'
 
 function plotLine() {
 	let line = Highcharts.chart('myLine', {
 		chart: {
-			type: "line"
+			type: "line",
+			backgroundColor: bcolor
 		},
 		title: {
 			text: 'Average Viewership Within 7 Days',
@@ -51,7 +53,8 @@ function plotLine() {
 function plotAgePie() {
 	var pie = Highcharts.chart('myAgePie', {
 		chart: {
-			type: "pie"
+			type: "pie",
+			backgroundColor: bcolor
 		},
 		title: {
 			text: 'Age',
@@ -110,7 +113,8 @@ function plotAgePie() {
 function plotGenderPie() {
 	var pie = Highcharts.chart('myGenderPie', {
 		chart: {
-			type: "pie"
+			type: "pie",
+			backgroundColor: bcolor
 		},
 		title: {
 			text: 'Gender',
@@ -168,6 +172,9 @@ function plotGenderPie() {
 
 function plotWordCloud(){
     let myWordCloud = Highcharts.chart('myWordCloud', {
+		chart: {
+			backgroundColor: bcolor
+		},
         accessibility: {
             screenReaderSection: {
                 beforeChartFormat: '<h5>{chartTitle}</h5>' +
@@ -215,77 +222,6 @@ function plotWordCloud(){
 }
 
 function plotBubble(){
-	// var width = 500, height = 500;
-	// var svg = d3.select('#myBubbleChart')
-	// 	.append('svg')
-	// 	.attr('height', height)
-	// 	.attr('width',width)
-	// 	.append('g')
-	// 	.attr("transform","translate(" + width/2 + "," + height/2 +")") // translate to center
-
-	// var defs = svg.append("defs") // add defs in svg
-	// defs.append("pattern") //add pattern in defs
-	// 	.attr("id","test")
-	// 	.attr("height","100%")
-	// 	.attr("weight","100%")
-	// 	.attr("patternContentUnits","objectBoundingBox")
-	// 	.append("image") //image in the pattern
-	// 	.attr("height",1)
-	// 	.attr("width",1)
-	// 	.attr("preserveAspectRatio","none")
-	// 	.attr("xmlns:xlink","http://www.w3.org/1999/xlink")
-	// 	.attr("xlink:href","images/frances.jpg");
-
-
-	// var radiusScale = d3.scaleSqrt()
-	// 	.domain([1,500]) //domain of data
-	// 	.range([10,80]) // radius of circles
-
-	// var simulation = d3.forceSimulation()
-	// 	.force("x", d3.forceX().strength(0.05)) // stay at center
-	// 	.force("y", d3.forceY().strength(0.05))
-	// 	.force("collide",d3.forceCollide(function(d){
-	// 		return radiusScale(d.followers) +1//pull circles apart based on radius of circle
-	// 	})) 
-
-	// d3.queue()
-	// 	.defer(d3.csv, "data/insta.csv")
-	// 	.await(ready)
-
-	// function ready (error, datapoints){
-	// 	//create patterns for circles
-	// 	defs.selectAll(".baker-pattern")
-	// 	.data(datapoints)
-	// 	.enter().append("pattern")
-	// 	.attr("class", "baker-pattern")
-	// 	.attr("id","test")
-
-	// 	//draw circles
-	// 	var circles = svg.selectAll(".artist")
-	// 		.data(datapoints)
-	// 		.enter().append("circle")
-	// 		.attr("class","artist")
-	// 		.attr("r",function(d){
-	// 			return radiusScale(d.followers)
-	// 		})
-	// 		.attr("fill","lightblue")
-	// 		.attr("stroke","black")
-
-
-	// simulation.nodes(datapoints)
-	// 	.on("tick",ticked)
-
-	// function ticked(){
-	// 	circles.
-	// 		attr("cx",function(d){
-	// 			return d.x
-	// 		})
-	// 		.attr("cy", function(d){
-	// 			return d.y
-	// 		})
-	// }
-	// }
-
 	var width = 500, height = 500;
 	var svg = d3.select('#myBubbleChart')
 		.append('svg')
@@ -420,7 +356,8 @@ function plotStackColumn(){
     let myStackedColumn = Highcharts.chart('myStackedColumn', {
         chart: {
             type: 'column',
-            animation: false
+			animation: false,
+			backgroundColor: bcolor
         },
         // title: {
         //     // text: 'Stacked column chart'
